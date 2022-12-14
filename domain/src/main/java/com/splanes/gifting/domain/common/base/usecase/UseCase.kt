f8f8f2f4f6d1646e9404ed70a2840c665b7e28c9
Sergi Planes <sergio.planes@adevinta.com>
2022-12-14 13:19:17 +0100

    feature(usecase-base): updated

--- domain/src/main/java/com/splanes/gifting/domain/common/base/usecase/UseCase.kt
+++ domain/src/main/java/com/splanes/gifting/domain/common/base/usecase/UseCase.kt
@@ -3,20 +3,24 @@ package com.splanes.gifting.domain.common.base.usecase
 import com.splanes.gifting.domain.common.error.GenericException
 import com.splanes.gifting.domain.common.error.KnownException
 import com.splanes.gifting.domain.common.error.TimeoutException
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.withTimeout
 import kotlin.coroutines.CoroutineContext
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 
 abstract class UseCase<Request, Response> {
 
     open val timeout: Duration = 30.seconds
     open val dispatcher: CoroutineContext = Dispatchers.IO
+
+    @Suppress("UNCHECKED_CAST")
+    suspend operator fun invoke() = (Unit as? Request)?.let { request ->
+        invoke(request)
+    } ?: error("Invoke requires params.")
 
     suspend operator fun invoke(request: Request): Flow<Result<Response>> = flow {
         try {
